83787535a91150106c21ad31162bcc3c6ed8f107
bumbleblym <bumbleblym@gmail.com>
2016-04-25 16:11:17 -0700

    chore(lint): Add ESLint React plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,19 @@
 module.exports = {
-  extends: 'google',
+  extends: [
+    'google',
+    'plugin:react/recommended'
+  ],
   env: {
     node: true,
-    mocha: true
+    mocha: true,
+    browser: true
   },
   globals: {
     expect: true
   },
+  plugins: [
+    "react"
+  ],
   rules: {
     'no-unused-expressions': 0
   }
